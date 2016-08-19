4f0ac4ff6a77d58a9360ec4b152449e416ba0a1f
Zvi 'Viz' Effron <viz+github@flippedperspective.com>
2016-08-18 18:19:23 -0700

    Wrap ID3D12Device

--- src/d3d12.rs
+++ src/d3d12.rs
@@ -8,6 +8,7 @@ use unknwn::*;
 use winapi;
 
 use std;
+use std::ops::Deref;
 
 pub fn d3d12_get_debug_interface<T : DeclspecUUID + From<*mut std::os::raw::c_void>>() -> Result<T, std::io::Error> {
     let mut debug : *mut std::os::raw::c_void = unsafe { std::mem::uninitialized() };
@@ -46,4 +47,58 @@ pub fn d3d12_create_device<T: DeclspecUUID + From<*mut std::os::raw::c_void>>(ad
          // switch to it
          result => panic!("{:x}", result),
      }
+}
+
+pub struct ID3D12Object {
+    ptr: *mut winapi::ID3D12Object,
+}
+
+impl ID3D12Object {
+}
+
+impl Deref for ID3D12Object {
+    type Target=IUnknown;
+
+    fn deref(&self) -> &IUnknown {
+        unsafe { &*(self as *const ID3D12Object as *const IUnknown) }
+    }
+}
+
+DEFINE_GUID! { ID3D12OBJECT_GUID, 0xc4fec28f, 0x7966, 0x4e95, 0x9f, 0x94, 0xf4, 0x31, 0xcb, 0x56, 0xc3, 0xb8 }
+
+impl DeclspecUUID for ID3D12Object {
+    fn uuid() -> winapi::GUID {
+        ID3D12OBJECT_GUID
+    }
+}
+
+pub struct ID3D12Device {
+    ptr: *mut winapi::ID3D12Device,
+}
+
+impl ID3D12Device {
+}
+
+impl Deref for ID3D12Device {
+    type Target=ID3D12Object;
+
+    fn deref(&self) -> &ID3D12Object {
+        unsafe { &*(self as *const ID3D12Device as *const ID3D12Object) }
+    }
+}
+
+impl From<*mut std::os::raw::c_void> for ID3D12Device {
+    fn from(source: *mut std::os::raw::c_void) -> Self {
+        ID3D12Device {
+            ptr: source as *mut winapi::ID3D12Device,
+        }
+    }
+}
+
+DEFINE_GUID! { ID3D12DEVICE_GUID, 0x189819f1, 0x1db6, 0x4b57, 0xbe, 0x54, 0x18, 0x21, 0x33, 0x9b, 0x85, 0xf7 }
+
+impl DeclspecUUID for ID3D12Device {
+    fn uuid() -> winapi::GUID {
+        ID3D12DEVICE_GUID
+    }
 }
